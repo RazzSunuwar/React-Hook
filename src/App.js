eaae093b6a3d3d9d6bc9adc8a53085f8d15b0864
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-08 22:59:53 +0545

    useContext Hook 1 & 2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ import MouseContainer from './components/MouseContainer';
 import IntervalClassCounter from './components/IntervalClassCounter';
 import IntervalHookCounter from './components/IntervalHookCounter';
 import DataFetching from './components/DataFetching';
+import ComponentC from './components/ComponentC';
 
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
 function App() {
   return (
     <div className="App">
@@ -29,7 +32,13 @@ function App() {
       {/* <MouseContainer /> */}
       {/* <IntervalClassCounter /> */}
       {/* <IntervalHookCounter /> */}
-      <DataFetching />
+      {/* <DataFetching /> */}
+
+      <UserContext.Provider value = 'Robert'>
+        <ChannelContext.Provider value= {'Coding World'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
       
 
 
