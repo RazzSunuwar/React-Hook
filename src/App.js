5bbeb44469b7d2f006c393abad0eef32849e8d93
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-08 19:58:16 +0545

    useEffect with incorrect dependency

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import HookCounterOne from './components/HookCounterOne';
 import ClassMouse from './components/ClassMouse';
 import HookMouse from './components/HookMouse';
 import MouseContainer from './components/MouseContainer';
+import IntervalClassCounter from './components/IntervalClassCounter';
+import IntervalHookCounter from './components/IntervalHookCounter';
 
 function App() {
   return (
@@ -23,7 +25,9 @@ function App() {
       {/* <HookCounterOne /> */}
       {/* <ClassMouse /> */}
       {/* < HookMouse /> */} 
-      <MouseContainer />
+      {/* <MouseContainer /> */}
+      <IntervalClassCounter />
+      <IntervalHookCounter />
       
 
 
