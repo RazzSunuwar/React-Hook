39697c6d1d9eb5cc921884ae73b95e96c68e6b27
raysunwar <ray@whitehatagency.com.au>
2020-04-08 11:42:34 +0545

    React Hooks - useEffect Hook

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import HookCounter from './components/HookCounter';
 import HookCounterTwo from './components/HookCounterTwo';
 import HookCounterThree from './HookCounterThree';
 import HooksCounterFour from './HooksCounterFour';
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne from './components/HookCounterOne';
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
       {/* <HookCounter /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounterThree /> */}
-      <HooksCounterFour />
+      {/* <HooksCounterFour /> */}
+      <ClassCounterOne />
+      <HookCounterOne />
       
 
 
