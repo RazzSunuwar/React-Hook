d4b5fbde91cf263a5adf01f68e2dd2240944057d
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-08 20:17:32 +0545

    useEffect with incorrect dependency

--- src/components/IntervalHookCounter.js
+++ src/components/IntervalHookCounter.js
@@ -4,15 +4,16 @@ function IntervalHookCounter() {
     const [ count, setCount ] = useState(0)
 
     const tick = () => {
-        setCount(count + 1);
+        setCount(prevCount => prevCount + 1);
     };
 
     useEffect(() => {
+
         const interval = setInterval(tick, 1000)
         return () => {
             clearInterval(interval)
         }
-    }, [count])
+    }, [])
     
     return (
         <div>
