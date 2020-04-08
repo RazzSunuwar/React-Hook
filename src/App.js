e494493bb4610b1cad37a8ce27b6c067f26f6c36
raysunwar <ray@whitehatagency.com.au>
2020-04-08 09:22:14 +0545

    React Hooks - useState with object

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import ClassComponent from './components/ClassComponent';
 import HookCounter from './components/HookCounter';
 import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './HookCounterThree';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassComponent /> */}
       {/* <HookCounter /> */}
-      <HookCounterTwo />
+      {/* <HookCounterTwo /> */}
+      <HookCounterThree />
+
 
     </div>
   );
